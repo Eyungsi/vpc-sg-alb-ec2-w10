c42fe2df6307d6c54d96861edc4754c3931a6dd6
Eyungsi <yuyunericyungsi@gmail.com>
2025-05-04 18:17:31 -0400

    modified backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "week10-statefile"
+    bucket       = "replace here with your bucket name"
     key          = "alb/terraform.state"
     region       = "us-east-1"
     encrypt      = true
