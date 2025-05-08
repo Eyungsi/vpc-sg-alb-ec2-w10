7fb608708f3e6a2570ca541c17005a9c98fb98a5
Eyungsi <yuyunericyungsi@gmail.com>
2025-05-08 14:07:59 -0400

    Action7

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 sudo dnf update -y
 sudo dnf install httpd -y
+sudo dnf install httpd unzip -y
+TEMP_DIR="/tmp/website"
+sudo mkdir -p "$TEMP_DIR"
 sudo mkdir -p /var/www/html
 sudo rm -rf /var/www/html/*
-if sudo cp -r /static-resume-main/* /var/www/html/; then
-    echo "Successfully copied website files"
-else
-    echo "Failed to copy website files" >&2
-    exit 1
-fi
+if [ -d "/tmp/static-resume-main" ]; then
+    sudo cp -r /tmp/static-resume-main/* /var/www/html/
 sudo chown -R apache:apache /var/www/html
 sudo find /var/www/html -type d -exec chmod 755 {} \;
 sudo find /var/www/html -type f -exec chmod 644 {} \;
