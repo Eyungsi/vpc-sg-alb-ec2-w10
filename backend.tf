e130f7666c64e72b64a85c8a76e6c7be0a463d73
Eyungsi <yuyunericyungsi@gmail.com>
2025-05-08 12:42:20 -0400

    Action6

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "week10-statefile"
-    key          = "alb/terraform.state"
-    region       = "us-east-1"
-    encrypt      = true
-   }
+    bucket  = "week10-statefile"
+    key     = "alb/terraform.state"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
