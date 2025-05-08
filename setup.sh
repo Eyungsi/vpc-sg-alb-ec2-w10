7d8b1b8bfa71db35e40369ea46e0085ad3a42e58
Eyungsi <yuyunericyungsi@gmail.com>
2025-05-08 14:56:31 -0400

    Action7

--- setup.sh
+++ setup.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 sudo dnf update -y -q
-sudo dnf install httpd -y
-sudo mkdir -p /var/www/html
+sudo dnf install httpd unzip wget -y
+wget https://github.com/utrains/static-resume/archive/refs/heads/main.zip
+unzip main.zip
 sudo rm -rf /var/www/html/*
-sudo cp -r /tmp/static-resume-main/* /var/www/html/
-sudo chown -R apache:apache /var/www/html
-sudo find /var/www/html -type d -exec chmod 755 {} \;
-sudo find /var/www/html -type f -exec chmod 644 {} \;
-sudo systemctl stop httpd
-sudo rm -rf /var/cache/httpd/*
+sudo cp -r static-resume-main/* /var/www/html/
 sudo systemctl start httpd
 sudo systemctl enable httpd
-sudo tee /var/www/html/version.txt <<EOF
-DEPLOYMENT_TIMESTAMP: $(date)
-COMMIT_HASH: ${GIT_COMMIT:-local}
-EOF
+
