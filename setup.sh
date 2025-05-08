1e40e6081afceb2641b265aa615d060c11716fbc
Eyungsi <yuyunericyungsi@gmail.com>
2025-05-08 15:15:35 -0400

    Action8

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-exec > /var/log/user-data.log 2>&1
-set -x
-sudo dnf update -y -q
+sudo dnf update -y
 sudo dnf install httpd unzip wget -y
 wget https://github.com/utrains/static-resume/archive/refs/heads/main.zip
 unzip main.zip
 sudo rm -rf /var/www/html/*
-sudo cp -r static-resume/* /var/www/html/
+sudo cp -r static-resume-main/* /var/www/html/
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
